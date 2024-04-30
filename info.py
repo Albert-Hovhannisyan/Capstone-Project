ea61882b9a1c7763964fc5fa50f8c74d519fdd6f
Albert Hovhannisyan <97188479+Albert-Hovhannisyan@users.noreply.github.com>
2024-04-30 17:57:30 +0400

    Update info.py

--- info.py
+++ info.py
@@ -33,19 +33,19 @@ text64 = str(text64)[2:-1]
 video_frame_start = random.randint(int(video.get_frame_count() / 8), int(video.get_frame_count() / 4))
 
 print(f'There are {video.get_frame_count()} frames in the video.')
-print(f'The FPS is {video.get_fps()}.\n')
+print(f'The frame rate is {video.get_fps()}.\n')
 
 print(f'There are {audio.get_frames()} samples in the audio.')
-print(f'The sample rate is {audio.get_sample_framerate()}.\n')
+print(f'The sample rate is {audio.get_sample_framerate()}Hz.\n')
 
 print(f'In one video frame there are approximately {int(audio.get_sample_framerate() / video.get_fps())} audio samples.\n')
 
-print(f'The inputs\'s text length is {len(text)}')
-print(f'The inputs\'s text length in base64 encoding is {len(text64)}\n')
+print(f'The input text length is {len(text)} characters')
+print(f'The input text length in base64 encoding is {len(text64)} characters\n')
 
-print(f'The number of audio samples needed to hide the data is {len(text64) * 3}\n')
+print(f'The number of audio samples needed to hide the data is {len(text64) * 3} samples\n')
 
-print(f'The data can be hidden in {video.get_frame_count() - video_frame_start} video frames')
+print(f'Totally the data can be hidden in {video.get_frame_count() - video_frame_start} video frames')
 print(f'The minimum amount of video frames in which the data will be hidden is {int(len(text64) / 100)}')
 
 file.close()
