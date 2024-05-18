17a1229f04c26b0c5560a834b4a4ee5598c0fcde
Albert Hovhannisyan <97188479+Albert-Hovhannisyan@users.noreply.github.com>
2024-05-18 20:24:12 +0400

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,14 @@ import numpy as np
 original_audio_path = "resources/audio.wav"
 stegano_audio_path = "resources/stegano.wav"
 
+bps = 16
+
 original = Audio(original_audio_path).get_sample_values()
 stegano = Audio(stegano_audio_path).get_sample_values()
 
 mse = np.mean(np.square(np.array(original) - np.array(stegano)))
 
-psnr = 10 * np.log10((max(original) ** 2) / mse)
+psnr = 10 * np.log10(((2**bps - 1) ** 2) / mse)
 
 print(f'The MSE is = {mse}')
 print(f'The PSNR is = {psnr}')
